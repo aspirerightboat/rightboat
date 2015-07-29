097ea92044630bdca2138b9596b1ca91b97b99d7
Chen ZhongXue <xmpolaris@gmail.com>
2015-07-30 00:19:39 +0800

    fix issue

--- app/jobs/solr_index_job.rb
+++ app/jobs/solr_index_job.rb
@@ -6,10 +6,13 @@ class SolrIndexJob
   end
 
   def perform
+    record = @klass.constantize.unscoped.find(@id)
     if @type.to_sym == :index
       record.solr_index_without_dj
     else
       record.solr_remove_from_index_without_dj
     end
+  rescue ActiveRecord::RecordNotFound
+    # Record destroyed?
   end
 end

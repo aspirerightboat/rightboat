797ce0c3edaab43c472b4afbf10efbc28237f181
Chen ZhongXue <xmpolaris@gmail.com>
2015-08-22 17:11:16 +0800

    stop boat description before 9 lines

--- app/helpers/boats_helper.rb
+++ app/helpers/boats_helper.rb
@@ -21,4 +21,16 @@ module BoatsHelper
   def favourite_link_to(boat, label = nil)
     link_to label || fav_label(boat), '#', class: fav_class(boat)
   end
+
+  def reduced_description(description)
+    length = vlength = 0
+    description.split('.').each do |x|
+      vlength += 50 if x =~ /<(br|h\d|p)>/
+      break if (vlength + x.length) > 410
+      vlength += (x.length + 1)
+      length += (x.length + 1)
+    end
+
+    sanitize(description[0..(length - 1)])
+  end
 end

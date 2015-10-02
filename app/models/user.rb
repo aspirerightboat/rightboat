78ca273c9b497a4d4df47d8e3fb953f5be52aeb1
Lev Lukomskyi <lllukom@gmail.com>
2015-10-02 19:07:43 +0300

    redundant

--- app/models/user.rb
+++ app/models/user.rb
@@ -111,8 +111,8 @@ class User < ActiveRecord::Base
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    if login = conditions.delete(:email)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    if (login = conditions.delete(:email))
+      where(conditions.to_h).where(['username = :value OR email = :value', {value: login}]).first
     else
       where(conditions.to_h).first
     end

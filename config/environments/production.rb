0f7b015b7cc4f42e54ea11bbeb12d9747e67d9b8
Lev Lukomskyi <lllukom@gmail.com>
2015-11-02 16:57:55 +0200

    fix sending emails on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,14 @@ Rails.application.configure do
 
   config.action_controller.default_url_options = { host: 'live.rightboat.com' }
   config.action_mailer.default_url_options = { host: 'live.rightboat.com' }
-  #
-  # config.action_controller.default_url_options = { host: 'www.rightboat.com' }
-  # config.action_mailer.default_url_options = { host: 'www.rightboat.com' }
-
-  # Disable actual deliveries
-  # Remove later
-  config.action_mailer.delivery_method = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      authentication: :plain,
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'rightboat.com',
+      user_name: 'boats@rightboat.com',
+      password: 'V6rB763-Zy',
+      enable_starttls_auto: true
+  }
 end

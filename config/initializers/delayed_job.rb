6475568d0a143ddeb18223dbf96e09346281d114
Lev Lukomskyi <lllukom@gmail.com>
2015-11-06 13:44:36 +0200

    give more time for workers

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,10 +1,12 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 5
-Delayed::Worker.max_attempts = 10 # period will be (retry ** 4)
-Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.max_attempts = 5 # period will be (retry ** 4)
+Delayed::Worker.max_run_time = 48.hours
 Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_priority = 0
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'),
+                                    5, # 5 files total
+                                    50*1024*1024) # 50 megabytes each

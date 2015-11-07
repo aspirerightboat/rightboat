fcc0d89d27cda837616fc5de6ce95ef1bd78db4f
xmpolaris <xmpolaris@gmail.com>
2015-11-07 17:06:18 +0800

    fix current search mode

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -58,7 +58,7 @@ class SearchController < ApplicationController
     search = Rightboat::BoatSearch.new(search_params)
     @boats = search.retrieve_boats
     session[:boats_count] = @boats.total_count
-    @prev_url = request.referrer[/boats-for-sale/]
+    @prev_url = request.referrer[/boats-for-sale/] if request.referer
 
     respond_to do |format|
       format.html

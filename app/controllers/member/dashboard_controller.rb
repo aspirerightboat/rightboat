c5740c5e5f14392343e1b4373399083da83ce4db
xmpolaris <xmpolaris@gmail.com>
2015-11-12 12:08:19 +0800

    add hide/unhide enquiries

--- app/controllers/member/dashboard_controller.rb
+++ app/controllers/member/dashboard_controller.rb
@@ -9,9 +9,5 @@ module Member
       current_user.build_address if current_user && current_user.address.nil?
       current_user.build_information if current_user && current_user.information.nil?
     end
-
-    def enquiries
-      @enquiries = current_user.enquiries.includes(boat: [:boat_type, :currency, :primary_image, :manufacturer, :model, :country, :vat_rate]).order(created_at: :desc)
-    end
   end
 end
---
+++ app/controllers/member/enquiries_controller.rb
@@ -0,0 +1,18 @@
+module Member
+  class EnquiriesController < BaseController
+    def index
+      @enquiries = current_user.enquiries.includes(boat: [:boat_type, :currency, :primary_image, :manufacturer, :model, :country, :vat_rate]).order(created_at: :desc)
+    end
+
+    def unhide
+      current_user.enquiries.update_all(deleted_at: nil)
+      render json: {}
+    end
+
+    def destroy
+      @enquiry = Enquiry.find(params[:id])
+      @enquiry.destroy
+      render json: {}
+    end
+  end
+end

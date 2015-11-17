305cf82c86679e8a280c948adeab0d5c39a97303
Lev Lukomskyi <lllukom@gmail.com>
2015-11-17 21:21:45 +0200

    changed default search ordering

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -31,7 +31,7 @@ class SearchController < ApplicationController
 
     params.delete(:page) unless request.xhr?
     params.delete(:boat_type) if params[:q].present?
-    set_current_search_order('score') if params[:q].present? && params[:order].blank?
+    set_current_search_order(params[:q].present? ? 'score' : 'price_desc') if params[:order].blank?
     params[:order] ||= current_search_order
 
     boat_search = Rightboat::BoatSearch.new.do_search(params, with_facets: true)

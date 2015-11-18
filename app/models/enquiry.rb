5416422a97b01867b24312be278385c5e4d93dc7
xmpolaris <xmpolaris@gmail.com>
2015-11-18 16:25:11 +0800

    change email validation for enquiry

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -15,7 +15,7 @@ class Enquiry < ActiveRecord::Base
   validates_presence_of :email, :boat_id, :token
   validates_presence_of :title, :first_name, :surname, :user, if: 'have_account == "0"'
   # validates_inclusion_of :title, within: User::TITLES, allow_blank: true
-  validates_format_of :email, with: /\A[^@]+@[^@]+\z/, allow_blank: true
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, allow_blank: true
   validates_uniqueness_of :token, allow_blank: true
 
   before_validation :generate_token

cde7cd454be1fabecd23273be648b6f20be942d9
xmpolaris <xmpolaris@gmail.com>
2015-11-30 18:15:54 +0800

    fix tax_status

--- app/models/vat_rate.rb
+++ app/models/vat_rate.rb
@@ -10,7 +10,7 @@ class VatRate < ActiveRecord::Base
 
   def tax_paid?(activated = nil)
     is_activated = activated.nil? ? true : activated
-    is_activated && !!(self.name.to_s =~ /inc vat|tax paid|vat paid/i)
+    is_activated && !!(self.name.to_s =~ /^tax paid|^paid|inc vat|vat paid/i)
   end
 
   def tax_status(activated = nil)

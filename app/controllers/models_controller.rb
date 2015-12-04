a1be13f8b045464cb66deffa5b63dc3185dfbec8
xmpolaris <xmpolaris@gmail.com>
2015-12-04 22:42:05 +0800

    show manufacturers/models having at least one boat

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,6 +1,11 @@
 class ModelsController < ApplicationController
   def index
-    @models = Model.order(:name).page(params[:page]).per(20)
+    @models = Model.joins(:boats).group('models.name, models.slug')
+                       .where('boats.deleted_at IS NULL')
+                       .order(:name).page(params[:page]).per(100)
+                       .select('models.name, models.slug, COUNT(*) AS boats_count')
+    @page = params[:page].try(:to_i)
+    @page = 1 if !@page || @page <= 0
   end
 
   def show

256346ed9c4de72bcd15a88d29b14f82e0c25080
xmpolaris <xmpolaris@gmail.com>
2015-12-22 18:30:38 +0800

    tweaks for not signed up

--- app/admin/not_signed_up.rb
+++ app/admin/not_signed_up.rb
@@ -1,27 +1,36 @@
-ActiveAdmin.register_page 'Not Signed Up' do
+ActiveAdmin.register Enquiry, as: 'Not Signed Up' do
 	menu parent: 'Users'
+  actions :index
 
-  content do
-    columns do
-      column do
-        panel 'Not Signed Up' do
-          table_for Enquiry.where(user_id: nil) do |t|
-            t.column('First Name') { |enquiry| enquiry.first_name }
-            t.column('Last Name') { |enquiry| enquiry.surname }
-            t.column('Email') { |enquiry| link_to enquiry.email, admin_lead_path(enquiry) }
-            t.column('Emailed') do |enquiry|
-            	if enquiry.email_sent
-            		'Yes'
-          		else
-          			form_for enquiry, url: admin_lead_path(enquiry), method: :put do |f|
-          				f.hidden_field :email_sent, value: true
-          				f.submit 'Sent'
-          			end
-          		end
-          	end
-          end
-        end
-      end
+  filter :first_name_or_surname_cont, as: :string, label: 'Name'
+
+  controller do
+    def scoped_collection
+      end_of_association_chain.where(user_id: nil).group(:email)
     end
+  end
+
+  index do
+    column :first_name
+    column :last_name, sortable: :surname do |enquiry|
+      enquiry.surname
+    end
+    column :email
+    column :date_of_first_lead, sortable: :created_at do |enquiry|
+      enquiry.created_at
+    end
+    column :telephone_number, sortable: :phone do |enquiry|
+      "#{enquiry.country_code} #{enquiry.phone}"
+    end
+    column :emailed, sortable: :email_sent do |enquiry|
+     if enquiry.email_sent
+       'Yes'
+       else
+         form_for enquiry, url: admin_lead_path(enquiry), method: :put do |f|
+           f.hidden_field :email_sent, value: true
+           f.submit 'Sent'
+         end
+       end
+     end
   end
 end

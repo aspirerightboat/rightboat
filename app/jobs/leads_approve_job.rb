6cd7b77400219995c21e1c80d06c096383a362e8
xmpolaris <xmpolaris@gmail.com>
2016-02-09 11:55:16 +0800

    ensure to create lead trail for automatic approved leads

--- app/jobs/leads_approve_job.rb
+++ app/jobs/leads_approve_job.rb
@@ -1,6 +1,6 @@
 class LeadsApproveJob
   def perform
     deadline = RBConfig[:leads_approve_delay].hours.ago
-    Enquiry.pending.where('created_at < ?', deadline).update_all(status: 'approved', updated_at: Time.current)
+    Enquiry.pending.where('created_at < ?', deadline).find_each { |x| x.update(status: 'approved', updated_at: Time.current) }
   end
 end

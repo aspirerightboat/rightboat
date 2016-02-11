d76d090228d0da538fe2fa4b8bd91a12beced60e
Lev Lukomskyi <lllukom@gmail.com>
2016-02-11 14:52:52 +0200

    link exports folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 set :linked_files, %w(config/database.yml config/secrets.yml config/application.yml config/smtp.yml public/sitemap.xml.gz public/robots.txt)
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads public/boat_images solr/data import_data)
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads public/boat_images public/exports solr/data import_data)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

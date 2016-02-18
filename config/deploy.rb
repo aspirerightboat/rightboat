71f0e9d0f3c5c2f02eb6a40724b4cb6401e8a1a3
Lev Lukomskyi <lllukom@gmail.com>
2016-02-18 02:34:17 +0200

    updated ruby to 2.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/upl
 set :keep_releases, 3
 
 set :rvm_type, :user
-set :rvm_ruby_version, '2.2.3'
+set :rvm_ruby_version, '2.3.0'
 
 
 # see: https://github.com/sgruhier/capistrano-db-tasks

17d2ecd5722a7dfec1499a5cd96c8464694be562
Lev Lukomskyi <lllukom@gmail.com>
2016-02-22 14:46:07 +0200

    added deleted status to array of possible lead statuses

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -1,6 +1,6 @@
 class Enquiry < ActiveRecord::Base
 
-  STATUSES = %w(pending quality_check approved rejected invoiced)
+  STATUSES = %w(pending quality_check approved rejected invoiced deleted)
   BAD_QUALITY_REASONS = %w(bad_contact contact_details_incorrect suspected_spam enquiry_received_twice other)
 
   # attr_accessor  #:captcha_correct

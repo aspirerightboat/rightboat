8969832f1b2a2a635fdd5977b409ef1287b2091d
xmpolaris <xmpolaris@gmail.com>
2016-03-17 11:02:18 +0800

    tweaks for saved search

--- app/models/saved_search.rb
+++ app/models/saved_search.rb
@@ -11,14 +11,14 @@ class SavedSearch < ActiveRecord::Base
     not_defined = '..'
     currency_sym = Currency.cached_by_name(currency).try(:symbol)
     res = ''
-    res << %( Keyword="#{q}") if q.present?
-    res << %( BoatType="#{boat_type}") if boat_type.present?
-    res << %( Manufacturer="#{manufacturer}") if manufacturer.present?
-    res << %( Model="#{model}") if model.present?
-    res << %( Year=#{year_min.presence || not_defined}-#{year_max.presence || not_defined}) if year_min.present? || year_max.present?
-    res << %( Price=#{currency_sym}#{price_min.presence || 0}-#{price_max.presence || not_defined}) if price_min.present? || price_max.present?
-    res << %( Length=#{length_min.presence || not_defined}-#{length_max.presence || not_defined}#{length_unit}) if length_min.present? || length_max.present?
-    res << %( RefNo="#{ref_no}") if ref_no.present?
+    res << %( Keyword = "#{q}") if q.present?
+    res << %( BoatType = "#{boat_type}") if boat_type.present?
+    res << %( Manufacturer = "#{manufacturer}") if manufacturer.present?
+    res << %( Model = "#{model}") if model.present?
+    res << %( Year = #{year_min.presence || not_defined} - #{year_max.presence || not_defined}) if year_min.present? || year_max.present?
+    res << %( Price = #{currency_sym}#{price_min.presence || 0} - #{price_max.presence || not_defined}) if price_min.present? || price_max.present?
+    res << %( Length = #{length_min.presence || not_defined} - #{length_max.presence || not_defined}#{length_unit}) if length_min.present? || length_max.present?
+    res << %( RefNo = "#{ref_no}") if ref_no.present?
     res.strip!
     res
   end
@@ -28,8 +28,10 @@ class SavedSearch < ActiveRecord::Base
   end
 
   def self.create_and_run(user, params)
-    ss = user.saved_searches.new(params)
-    ss.first_found_boat_id = Rightboat::BoatSearch.new.do_search(params, per_page: 1).hits.first.try(:primary_key)
-    ss.save!
+    if user.saved_searches.where(params).blank?
+      ss = user.saved_searches.new(params)
+      ss.first_found_boat_id = Rightboat::BoatSearch.new.do_search(params, per_page: 1).hits.first.try(:primary_key)
+      ss.save!
+    end
   end
 end

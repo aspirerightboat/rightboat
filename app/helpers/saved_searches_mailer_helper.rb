6241623f36a9e80678771c564231a11b00b3f504
Lana Dzyuban <svitlana.dzyuban@gmail.com>
2016-04-12 13:30:56 +0300

    Update saved_searches_mailer_helper.rb
    remove operator

--- app/helpers/saved_searches_mailer_helper.rb
+++ app/helpers/saved_searches_mailer_helper.rb
@@ -1,6 +1,6 @@
 module SavedSearchesMailerHelper
   def track_email_open_saved_searches(saved_searches_alert)
-    url = saved_search_opened_email_trackings_url(token: saved_searches_alert&.token, format: 'png')
+    url = saved_search_opened_email_trackings_url(token: saved_searches_alert.token, format: 'png')
     image_tag(url, style: 'display: none', size: '1x1')
   end
 end

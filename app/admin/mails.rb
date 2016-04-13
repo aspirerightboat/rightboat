2d0a1e2b9d6daad645057bcb02a1410eb7e79a1e
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2016-04-13 11:56:00 +0300

    Filters for Mail stats

--- app/admin/mails.rb
+++ app/admin/mails.rb
@@ -1,13 +1,25 @@
 ActiveAdmin.register_page 'Mails' do
 
   menu label: 'Mails'
+
   content title: "Mails" do
+
     columns do
       column do
-        total_sent = SavedSearchesAlert.count
-        total_opened = SavedSearchesAlert.where('opened_at IS NOT NULL').count
-        total_clicked = MailClick.count
-        total_leads = Enquiry.where('saved_searches_alert_id IS NOT NULL').count
+        @created_at_gteq = (Time.current - 1.week).to_date.to_s(:db)
+        @created_at_lteq = (Time.current).to_date.to_s(:db)
+
+        if params[:q]
+          @created_at_lteq = params[:q][:created_at_lteq]
+          @created_at_gteq = params[:q][:created_at_gteq]
+        end
+
+        render partial: 'filters', locals: {created_at_gteq: @created_at_gteq, created_at_lteq: @created_at_lteq}
+
+        total_sent = SavedSearchesAlert.where(created_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
+        total_opened = SavedSearchesAlert.where('opened_at IS NOT NULL').where(opened_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
+        total_clicked = MailClick.where(created_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
+        total_leads = Enquiry.where('saved_searches_alert_id IS NOT NULL').where(updated_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
 
         render partial: 'stats', locals: {
             total_sent: total_sent,

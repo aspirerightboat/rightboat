3d9600520a8ec21a71a114875b61aabbb3fa86ad
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2016-04-15 18:10:55 +0300

    - Sorting by mail Id for leads - Add link to mails stats lead count - Change leads's query to select by updated_at

--- app/admin/mails.rb
+++ app/admin/mails.rb
@@ -19,13 +19,15 @@ ActiveAdmin.register_page 'Mails' do
         total_sent = SavedSearchesAlert.where(created_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
         total_opened = SavedSearchesAlert.where('opened_at IS NOT NULL').where(opened_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
         total_clicked = MailClick.where(created_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
-        total_leads = Enquiry.where('saved_searches_alert_id IS NOT NULL').where(updated_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
+        total_leads = Enquiry.where('saved_searches_alert_id IS NOT NULL').where(created_at: @created_at_gteq.to_date.beginning_of_day.. @created_at_lteq.to_date.end_of_day).count
 
         render partial: 'stats', locals: {
             total_sent: total_sent,
             total_opened: total_opened,
             total_clicked: total_clicked,
-            total_leads: total_leads
+            total_leads: total_leads,
+            created_at_gteq: @created_at_gteq,
+            created_at_lteq: @created_at_lteq
         }
       end
     end

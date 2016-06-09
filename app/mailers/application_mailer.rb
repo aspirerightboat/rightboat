ad9f3573921124d795ea11bbfb90ed0e9e37355d
Lev Lukomskyi <lllukom@gmail.com>
2016-06-09 15:21:48 +0300

    temporarily remove Don from error mails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,7 +5,7 @@ class ApplicationMailer < ActionMailer::Base
   add_template_helper MailerHelper
 
   STAGING_EMAIL = ('info@rightboat.com' if Rails.env.staging?)
-  DEVELOPER_EMAILS = %w(don.fuller@cotoco.com llukomskyy@n-ix.com xmpolaris@hotmail.com)
+  DEVELOPER_EMAILS = %w(llukomskyy@n-ix.com xmpolaris@hotmail.com)
 
   private
 

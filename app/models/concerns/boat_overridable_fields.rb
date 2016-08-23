a2c5ccad38063d87a87b2145e97d9c7fa7a48c50
Lev Lukomskyi <lllukom@gmail.com>
2016-08-23 18:14:46 +0300

    hotfix ActiveModel::MissingAttributeError

--- app/models/concerns/boat_overridable_fields.rb
+++ app/models/concerns/boat_overridable_fields.rb
@@ -11,41 +11,69 @@ module BoatOverridableFields
 
     OVERRIDABLE_FIELDS.each do |attr_name|
       define_method "#{attr_name}=" do |value|
-        assign_overridable_field(attr_name, value)
+        if Import.import_running?
+          if raw_boat
+            if send(attr_name).presence == raw_boat.send(attr_name).presence
+              super(value)
+            end
+            raw_boat.send("#{attr_name}=", value)
+          else
+            super(value)
+          end
+        else
+          if import_id && !expert_boat? && !raw_boat
+            build_raw_boat(attributes.slice(*OVERRIDABLE_FIELDS))
+          end
+          super(value)
+        end
       end
       if attr_name.end_with?('_id')
-        association = attr_name.chomp('_id')
-        define_method "#{association}=" do |value|
-          assign_overridable_field(association, value)
+        attr_name = attr_name.chomp('_id')
+        define_method "#{attr_name}=" do |value|
+          if Import.import_running?
+            if raw_boat
+              if send(attr_name).presence == raw_boat.send(attr_name).presence
+                super(value)
+              end
+              raw_boat.send("#{attr_name}=", value)
+            else
+              super(value)
+            end
+          else
+            if import_id && !expert_boat? && !raw_boat
+              build_raw_boat(attributes.slice(*OVERRIDABLE_FIELDS))
+            end
+            super(value)
+          end
         end
       end
     end
 
-    def assign_overridable_field(attr_name, value)
-      if Import.import_running?
-        assign_imported_value(attr_name, value)
-      else
-        override_imported_value(attr_name, value)
-      end
-    end
-
-    def assign_imported_value(attr_name, value)
-      if raw_boat
-        if send(attr_name).presence == raw_boat.send(attr_name).presence
-          self[attr_name] = value
-        end
-        raw_boat.send("#{attr_name}=", value)
-      else
-        self[attr_name] = value
-      end
-    end
-
-    def override_imported_value(attr_name, value)
-      if import_id && !expert_boat? && !raw_boat
-        build_raw_boat(attributes.slice(*OVERRIDABLE_FIELDS))
-      end
-      self[attr_name] = value
-    end
+    # def assign_overridable_field(attr_name, value)
+    #   if Import.import_running?
+    #     assign_imported_value(attr_name, value)
+    #   else
+    #     override_imported_value(attr_name, value)
+    #   end
+    # end
+    #
+    # def assign_imported_value(attr_name, value)
+    #   if raw_boat
+    #     if send(attr_name).presence == raw_boat.send(attr_name).presence
+    #       self[attr_name] = value
+    #     end
+    #     raw_boat.send("#{attr_name}=", value)
+    #   else
+    #     self[attr_name] = value
+    #   end
+    # end
+    #
+    # def override_imported_value(attr_name, value)
+    #   if import_id && !expert_boat? && !raw_boat
+    #     build_raw_boat(attributes.slice(*OVERRIDABLE_FIELDS))
+    #   end
+    #   self[attr_name] = value
+    # end
 
     def imported_field_value(attr_name)
       raw_boat ? raw_boat.send(attr_name) : send(attr_name)

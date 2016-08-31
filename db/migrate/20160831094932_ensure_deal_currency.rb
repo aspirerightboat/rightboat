7be3ed356db90dd10486a7efd6a71b355f200801
Lev Lukomskyi <lllukom@gmail.com>
2016-08-31 15:10:41 +0300

    improve

--- db/migrate/20160831094932_ensure_deal_currency.rb
+++ db/migrate/20160831094932_ensure_deal_currency.rb
@@ -1,6 +1,6 @@
 class EnsureDealCurrency < ActiveRecord::Migration
   def up
-    Deal.includes(:user).where(currency_id: nil).each do |deal|
+    Deal.includes(user: {address: :country}).where(currency_id: nil).each do |deal|
       currency = Currency.deal_currency_by_country(deal.user.country&.iso)
       deal.update(currency: currency)
     end

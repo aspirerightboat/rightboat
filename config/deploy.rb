34385cac8f47fd86c399b34f5f60cefefbd935c3
Lev Lukomskyi <lllukom@gmail.com>
2016-10-10 17:34:00 +0300

    updated capistrano gem1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.6.1'
 
 set :application, 'rightboat_v2'
 set :repo_url, 'git@github.com:OxygenCapitalLimited/rightboat.git'

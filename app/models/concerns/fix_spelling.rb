61726e56255b6d145dce73815b03e450bb6a10ba
Lev Lukomskyi <lllukom@gmail.com>
2016-10-13 19:39:17 +0300

    cosmetic

--- app/models/concerns/fix_spelling.rb
+++ app/models/concerns/fix_spelling.rb
@@ -2,7 +2,6 @@ module FixSpelling
   extend ActiveSupport::Concern
 
   included do
-
     has_many :misspellings, as: :source, dependent: :destroy
 
     scope :query_with_aliases, -> (value) {
@@ -10,23 +9,22 @@ module FixSpelling
       q = q.where("#{table_name}.name = :name OR misspellings.alias_string = :name", name: value)
       q.create_with(name: value)
     }
-
-    def merge_into(target)
-      self.class.transaction do
-        target.misspellings.where(alias_string: self.name).first_or_create!
-        self.misspellings.each do |misspelling|
-          target.misspellings.where(alias_string: misspelling.alias_string).first_or_create!
-        end
-        self.class.reflections.each do |name, reflection|
-          macro = reflection.macro.to_s
-          if macro =~ /has_/ && !reflection.options[:through]
-            self.send(name.to_sym).update_all(reflection.foreign_key => target.id)
-          end
-        end
-        self.destroy
-      end
-
-    end
-
   end
+
+  def merge_into(target)
+    self.class.transaction do
+      target.misspellings.where(alias_string: name).first_or_create!
+      misspellings.each do |misspelling|
+        target.misspellings.where(alias_string: misspelling.alias_string).first_or_create!
+      end
+      self.class.reflections.each do |name, reflection|
+        macro = reflection.macro.to_s
+        if macro =~ /has_/ && !reflection.options[:through]
+          send(name.to_sym).update_all(reflection.foreign_key => target.id)
+        end
+      end
+      destroy
+    end
+  end
+
 end

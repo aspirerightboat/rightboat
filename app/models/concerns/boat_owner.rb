d21aba748b0114b085b394a4053a2d93615df910
Lev Lukomskyi <lllukom@gmail.com>
2016-10-13 19:34:51 +0300

    boat_owner refactoring

--- app/models/concerns/boat_owner.rb
+++ app/models/concerns/boat_owner.rb
@@ -2,18 +2,18 @@ module BoatOwner
   extend ActiveSupport::Concern
 
   included do
-
     has_many :boats, inverse_of: self.name.underscore, dependent: :restrict_with_error
 
-    before_destroy :ensure_boats
-
-    private
-
-      def ensure_boats
-        unless status = Boat.where("#{self.class.name.underscore}_id = ?", id).empty?
-          errors.add :base, 'Cannot delete with boats'
-        end
-        status
-      end
+    before_destroy :ensure_no_boat_dependencies
   end
+
+  private
+
+  def ensure_no_boat_dependencies
+    if Boat.where("#{self.class.name.underscore}_id = ?", id).exists?
+      errors.add :base, 'Cannot delete with boats'
+      throw :abort
+    end
+  end
+
 end

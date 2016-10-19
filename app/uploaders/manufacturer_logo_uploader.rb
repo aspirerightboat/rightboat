7e7371852e3b55ee3e4a4730433fab8d686761b7
Lev Lukomskyi <lllukom@gmail.com>
2016-10-19 13:29:57 +0300

    link directory manufacturer logos on staging

--- app/uploaders/manufacturer_logo_uploader.rb
+++ app/uploaders/manufacturer_logo_uploader.rb
@@ -1,4 +1,9 @@
 class ManufacturerLogoUploader < ImageUploader
+
+  def store_dir
+    "manufacturer_logos/#{model.id}"
+  end
+
   version :thumb do
     process :resize_and_pad => [300, 200]
   end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -12,4 +12,4 @@ set :solr_process_regexp, 'java .*solr.* start\.jar'
 set :solr_cmd, "/home/ubuntu/.rvm/bin/rvm-shell -c 'cd #{current_path} && RAILS_ENV=staging bundle exec sunspot-solr %{cmd} --port=8983 --solr-home=#{current_path}/solr --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids'"
 
 set :linked_dirs, fetch(:linked_dirs) + %w(public/boat_images public/broker_logos public/user
-                                           public/buyer_guide public/article_images public/zipped_pdfs)
+                                           public/buyer_guide public/article_images public/manufacturer_logos)

17bbf105939c19783b8b633a041cf565d04f87e5
Lev Lukomskyi <lllukom@gmail.com>
2016-10-25 20:36:46 +0300

    admin view fix

--- app/admin/google_dynamic_remarketing.rb
+++ app/admin/google_dynamic_remarketing.rb
@@ -2,12 +2,12 @@ ActiveAdmin.register_page 'Google Dynamic Remarketing' do
   menu parent: 'Other', priority: 80
 
   content title: 'Google Dynamic Remarketing' do
-    para {
-      link_to 'CSV feed', Rightboat::GoogleDynamicRemarketing.csv_path, target: '_blank'
-    }
-    para {
-      "Last updated: <b>#{File.mtime(Rightboat::GoogleDynamicRemarketing.csv_fullpath)}</b>".html_safe
-    }
+    file_path = Rightboat::GoogleDynamicRemarketing.csv_fullpath
+
+    if File.exist?(file_path)
+      para { link_to 'CSV feed', Rightboat::GoogleDynamicRemarketing.csv_path, target: '_blank' }
+      para { "Last updated: <b>#{File.mtime(file_path)}</b>".html_safe }
+    end
     para {
       link_to 'Generate', {action: :generate_csv}, method: :post, class: 'button', data: {disable_with: 'Working...'}
     }

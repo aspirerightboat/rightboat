3be174405a832e49b612cec800fe9a44f2275a0b
xmpolaris <xmpolaris@gmail.com>
2016-10-26 17:59:47 +0800

    fix payment method present method #1083

--- app/models/user.rb
+++ app/models/user.rb
@@ -144,7 +144,7 @@ class User < ApplicationRecord
   end
 
   def payment_method_present?
-    !broker_info.payment_method =~ /none/
+    !(broker_info.payment_method =~ /none/)
   end
 
   def lead_emails_active?

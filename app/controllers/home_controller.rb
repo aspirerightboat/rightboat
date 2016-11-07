130588c6f91296a2acca1947ce0d6edee8efc4d7
Lev Lukomskyi <lllukom@gmail.com>
2016-11-07 14:27:35 +0200

    added more manufacturers button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,6 @@ class HomeController < ApplicationController
     if params[:popup_login] && user_signed_in?
       redirect_to(root_path) and return
     end
-
-    @featured_manufacturers = Manufacturer.featured.logo_present.order(:name).limit(60).to_a
-    load_recent_boats
   end
 
   def contact
@@ -51,25 +48,6 @@ class HomeController < ApplicationController
   end
 
   private
-
-  def load_recent_boats
-    if current_user
-      boat_ids = current_user.user_activities.where(kind: 'boat_view')
-                     .order('id DESC').group(:boat_id).limit(3).pluck(:boat_id)
-      if boat_ids.empty? && cookies[:recently_viewed_boat_ids]
-        boat_ids = Boat.where(id: cookies.delete(:recently_viewed_boat_ids).split(',')).limit(3).pluck(:id)
-        boat_ids.each { |boat_id| UserActivity.create_boat_visit(boat_id: boat_id, user: current_user) }
-      end
-      if boat_ids.any?
-        @recent_boats = Boat.active.where(id: boat_ids)
-                            .includes(:manufacturer, :model, :country, :currency, :primary_image).to_a
-      end
-    elsif cookies[:recently_viewed_boat_ids]
-      boat_ids = cookies[:recently_viewed_boat_ids].split(',')
-      @recent_boats = Boat.active.where(id: boat_ids)
-                          .includes(:currency, :manufacturer, :model, :country, :primary_image).to_a
-    end
-  end
 
   def load_visited
     if !cookies[:visited]
